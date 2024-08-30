adcc5ed440b70f26edb3a86de209ca6897f3e7f4
yigex <yigex@amd.com>
2024-08-30 09:43:07 +0800

    Update and rename run.sh to run_vllm.sh

--- Inference/offline/latency/run_vllm.sh
+++ Inference/offline/latency/run_vllm.sh
@@ -11,7 +11,7 @@ if [ $1 -eq 1 ];then
                 for input_len in 2000;
                 do
                 echo "=======RUNNING $2 $input_len $gen_len ========="
-                torchrun --standalone --nproc_per_node=$3 --nnodes=1 $VLLM_DIR/benchmarks/benchmark_latency.py --model $2 --input-len $input_len --output-len $gen_len --batch-size $4 --tensor-parallel-size 1 --num-iters 5 --profile --profile-result-dir $PWD
+                torchrun --standalone --nproc_per_node=$4 --nnodes=1 $VLLM_DIR/benchmarks/benchmark_latency.py --model $2 --input-len $input_len --output-len $gen_len --batch-size $5 --tensor-parallel-size $4 --num-iters 5 --dtype $3 --profile --profile-result-dir $PWD
         #python benchmarks/benchmark_latency.py --model $MODEL --input-len $input_len --output-len $gen_len --batch-size 1  --tensor-parallel-size 1 --num-iters 5
                 done
         done
@@ -21,7 +21,7 @@ else
                 for input_len in 1024 4096 8192 32768;
                 do
                 echo "=======RUNNING $2 $input_len $gen_len ========="
-                torchrun --standalone --nproc_per_node=$3 --nnodes=1 $VLLM_DIR/benchmarks/benchmark_latency.py --model $2 --input-len $input_len --output-len $gen_len --batch-size $4 --tensor-parallel-size $3 --num-iters 3    
+                torchrun --standalone --nproc_per_node=$4 --nnodes=1 $VLLM_DIR/benchmarks/benchmark_latency.py --model $2 --input-len $input_len --output-len $gen_len --batch-size $5 --tensor-parallel-size $4 --dtype $3 --num-iters 3 
                 done
         done
 
