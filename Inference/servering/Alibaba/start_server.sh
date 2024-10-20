3314fefb9f449e66e1918ee9ec36a9aec7873b2f
yigex <yigex@amd.com>
2024-10-20 16:19:36 +0800

    Update start_server.sh

--- Inference/servering/Alibaba/start_server.sh
+++ Inference/servering/Alibaba/start_server.sh
@@ -2,9 +2,9 @@
 export OPENBLAS_NUM_THREADS=1
 
 if [ $1 = "mixtral" ]; then
-        HIP_VISIBLE_DEVICES=0,1 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Mixtral-8x22B-v0.1/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 1 --dtype float16 --distributed-executor-backend mp
+        HIP_VISIBLE_DEVICES=0,1 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Mixtral-8x22B-v0.1/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 1 --dtype float16 --distributed-executor-backend mp --enable-chunked-prefill=False
 elif [ $1 = "72b" ]; then
-        HIP_VISIBLE_DEVICES=0,1 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Qwen2-72B/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 2 --dtype float16 --distributed-executor-backend mp
+        HIP_VISIBLE_DEVICES=0,1 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Qwen2-72B/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 2 --dtype float16 --distributed-executor-backend mp --enable-chunked-prefill=False
 else
-        HIP_VISIBLE_DEVICES=0 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Qwen2-7B/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 1 --dtype float16 --distributed-executor-backend mp
+        HIP_VISIBLE_DEVICES=0 python -m vllm.entrypoints.sync_openai.api_server --host 0.0.0.0 --port 8011 --model /data/Qwen2-7B/ --swap-space 16 --disable-log-requests --trust-remote-code --tensor-parallel-size 1 --dtype float16 --distributed-executor-backend mp --enable-chunked-prefill=False
 fi
